16625299c926d091d4a044636eeb344e60a4a814
Lucas Serafim <lslucas@gmail.com>
2011-02-17 16:20:43 -0600

    meu site

---
+++ js/inc.base.js
@@ -0,0 +1,13 @@
+    $(function(){
+
+	/* LAYOUT
+	******************** */
+	 // centraliza tudo
+	 $('.container').center();
+	 
+	 // tip
+	 $(".tip").tipTip({maxWidth: "auto", edgeOffset: 10});
+
+	/* //LAYOUT
+	******************** */
+    });
